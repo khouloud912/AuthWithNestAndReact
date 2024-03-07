3bd45e17d6550ca736f8acf1d0399fe17e527017
khouloud912 <60706637+khouloud912@users.noreply.github.com>
2024-03-07 09:13:35 +0100

    setup env file

--- back-app/src/app.module.ts
+++ back-app/src/app.module.ts
@@ -5,20 +5,27 @@ import { AuthModule } from './auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
 import { User } from './user/entities/user.entity';
-import { ConfigModule } from '@nestjs/config';
-import * as Joi from 'joi';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [ TypeOrmModule.forRoot({
-    type: 'mysql',
-    host: 'localhost',
-    port: 3307,
-    username: 'root',
-    password: 'root',
-    database: 'test',
-    entities: [User],
-    synchronize: true,
-  }),AuthModule, UserModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get<string>('DB_HOST'),
+        port: parseInt(configService.get<string>('DB_PORT')),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_DATABASE'),
+        entities: [User],
+        synchronize: true,
+      }),
+    }),
+    AuthModule,
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
